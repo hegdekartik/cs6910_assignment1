038b2f428044fe18bbb46549dbb7b46b729e13af
vidya-siva <100091275+vidya-siva@users.noreply.github.com>
2022-02-24 19:24:14 +0530

    helper function

---
+++ helper.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class OHE():
+    def __init__(self):
+        pass
+
+    def oneHotEncode(self,num_classes, Y_train_raw):
+        Ydata = np.zeros((num_classes, Y_train_raw.shape[0]))
+        for i in range(Y_train_raw.shape[0]):
+            value = Y_train_raw[i]
+            Ydata[int(value)][i] = 1.0
+        return Ydata
